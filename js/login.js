be739a0d86732d2eb225c01a6b4d1b34286e2b29
Shaked-BA <shaked.ba3@gmail.com>
2023-04-10 21:01:58 +0300

    begun implement the validation of login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,14 @@
+var players = {"p": "testuser"};
+
 // Get the button element from the DOM
 var loginButton = document.getElementById("login-btn");
 
 // Add an event listener to the button
 loginButton.addEventListener("click", function() {
+    if (!validate()) {
+        alert("Wrong username or password.");
+        return;
+    }
     // Space starts the game.
     game.level = 1;
     game.score = 0;
@@ -10,3 +16,13 @@ loginButton.addEventListener("click", function() {
     game.moveToState(new LevelIntroState(game.level));
     setVisibility('login', 'none');
 });
+
+function validate() {
+    var un = document.getElementById("username").value;
+    var lp = document.getElementById("lpassword").value;
+
+    if (un in players && players[un] === lp) {
+        return true;
+    }
+    return false;
+}
