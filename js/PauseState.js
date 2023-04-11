ae4ff1c2024cbcb635a5b3726dd33047f946b929
Shaked-BA <shaked.ba3@gmail.com>
2023-04-11 14:14:03 +0300

    edited pause

--- js/PauseState.js
+++ js/PauseState.js
@@ -15,10 +15,14 @@ PauseState.prototype.draw = function(game, dt, ctx) {
     //  Clear the background.
     ctx.clearRect(0, 0, game.width, game.height);
 
-    ctx.font="14px Arial";
-    ctx.fillStyle = '#ffffff';
-    ctx.textBaseline="middle";
-    ctx.textAlign="center";
-    ctx.fillText("Paused", game.width / 2, game.height/2);
+    var photo = new Image();
+    photo.src = 'images/puase.png';
+    ctx.drawImage(photo, game.width / 5, game.height / 7, 500, 500);
+
+    // ctx.font="14px Arial";
+    // ctx.fillStyle = '#ffffff';
+    // ctx.textBaseline="middle";
+    // ctx.textAlign="center";
+    // ctx.fillText("Paused", game.width / 2, game.height/2);
     return;
 };
